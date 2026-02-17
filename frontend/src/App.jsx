1bc27bddcf61911ba95ea7435b90034c9cadecdb
Anuj <anujsharma135790@gmail.com>
2026-02-18 04:16:38 +0530

    Add protected routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -50,7 +50,7 @@
 // export default App;
 
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate } from "react-router-dom";
 import io from "socket.io-client";
 
 import Signup from "./components/Signup.jsx";
