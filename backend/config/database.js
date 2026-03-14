6339b9e31524192061f6277185b19152b4ee09a2
Anuj <anujsharma135790@gmail.com>
2026-03-15 04:21:53 +0530

    Something

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,15 +2,17 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to:", process.env.MONGO_URL); // 👈 add this
+
     await mongoose.connect(process.env.MONGO_URL).then(() => {
       console.log("MongoDB connected successfully");
-    });//dont use await and then together, it is redundant. You can choose either one. If you use await, you can simply write:
+    }); //dont use await and then together, it is redundant. You can choose either one. If you use await, you can simply write:
     // await mongoose.connect(process.env.MONGO_URL);
     // console.log("MongoDB connected successfully");
   } catch (error) {
     console.error("MongoDB connection failed:", error);
     process.exit(1); // Exit the process with failure
-  } 
+  }
 };
 
 module.exports = connectDB;
